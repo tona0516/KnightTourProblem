fe4b3abe2a0c8d85d02e979ff2ddf69c0dcbd134
tona0516 <tonango.0516.gmail.com>
2019-06-23 14:26:13 +0900

    feat: closedが解けるよう修正

--- knight_tour_problem.py
+++ knight_tour_problem.py
@@ -1,102 +1,75 @@
 import copy
 
-MASS_NUM = 6
+MASS_NUM = 8
 
 
 def init_board():
     mass = {
-        'is_pass': False,
-        'order': -1,
+        'is_passed': False,
+        'order': 0,
     }
     return [[copy.deepcopy(mass) for i in range(MASS_NUM)] for j in range(MASS_NUM)]
 
 
 def get_initial_position():
-    # TODO ランダムにする
-    return (2, 2)
+    return (0, 0)
 
 
 def is_finished(board, current_posiotion, initial_position):
+    # 現在位置から初期位置に戻れること
+    next_positions = get_next_positions(board, current_posiotion)
+    if not (initial_position in next_positions):
+        return False
+
+    # すべて通過していること
     for row in board:
         for mass in row:
-            if mass['is_pass'] is False:
+            if mass['is_passed'] is False:
                 return False
+
     return True
 
 
-def can_move_initial_position(board, initial_position):
-    tmp = []
-    # -1, 1, 2, -2の組み合わせ
-    tmp.append((initial_position[0] + 1, initial_position[1] + 2))
-    tmp.append((initial_position[0] + 1, initial_position[1] - 2))
-    tmp.append((initial_position[0] - 1, initial_position[1] + 2))
-    tmp.append((initial_position[0] - 1, initial_position[1] - 2))
-    tmp.append((initial_position[0] + 2, initial_position[1] + 1))
-    tmp.append((initial_position[0] + 2, initial_position[1] - 1))
-    tmp.append((initial_position[0] - 2, initial_position[1] + 1))
-    tmp.append((initial_position[0] - 2, initial_position[1] - 1))
-
-    next_positions = []
-    for np in tmp:
-        # マスに収まっていない位置を削除
-        if not (0 <= np[0] < MASS_NUM and 0 <= np[1] < MASS_NUM):
-            continue
-        if board[np[0]][np[1]]['is_pass'] is False:
-            return True
-
-    return False
-
 def get_next_positions(board, current_position):
-    tmp = []
-    # -1, 1, 2, -2の組み合わせ
-    tmp.append((current_position[0] + 1, current_position[1] + 2))
-    tmp.append((current_position[0] + 1, current_position[1] - 2))
-    tmp.append((current_position[0] - 1, current_position[1] + 2))
-    tmp.append((current_position[0] - 1, current_position[1] - 2))
-    tmp.append((current_position[0] + 2, current_position[1] + 1))
-    tmp.append((current_position[0] + 2, current_position[1] - 1))
-    tmp.append((current_position[0] - 2, current_position[1] + 1))
-    tmp.append((current_position[0] - 2, current_position[1] - 1))
-
-    next_positions = []
-    for np in tmp:
-        # マスに収まっていない位置を削除
-        if not (0 <= np[0] < MASS_NUM and 0 <= np[1] < MASS_NUM):
-            continue
-        # すでに通ったマスを削除
-        if board[np[0]][np[1]]['is_pass'] is True:
-            continue
-
-        next_positions.append(np)
-
-    # print(str(board[current_position[0]][current_position[1]]['order']) + ': ' + str(current_position) + ' -> ' + str(next_positions))
+    diffs = [
+        (1, 2),
+        (1, -2),
+        (-1, 2),
+        (-1, -2),
+        (2, 1),
+        (2, -1),
+        (-2, 1),
+        (-2, -1),
+    ]
+    tmp = [(current_position[0] + diff[0], current_position[1] + diff[1]) for diff in diffs]
+    next_positions = [np for np in tmp if 0 <= np[0] < MASS_NUM and 0 <= np[1] < MASS_NUM]
     return next_positions
 
 
-def solve(board, current_position, initial_position):
-    if not can_move_initial_position(board, initial_position):
-        return None
+def get_jumpable_positions(board, current_position):
+    next_positions = get_next_positions(board, current_position)
+    jumpable_positions = [np for np in next_positions if board[np[0]][np[1]]['is_passed'] is False]
+    return jumpable_positions
 
+
+def solve(board, current_position, initial_position):
     if is_finished(board, current_position, initial_position):
         return board
 
-    # print('---- board ----')
-    # print_board(board)
-
-    next_positions = get_next_positions(board, current_position)
+    next_positions = get_jumpable_positions(board, current_position)
     if not next_positions:
         return None
 
     sorted_next_positions = []
     for np in next_positions:
-        sorted_next_positions.append((len(get_next_positions(board, np)), np))
+        sorted_next_positions.append((len(get_jumpable_positions(board, np)), np))
 
     sorted_next_positions = [np[1] for np in sorted(sorted_next_positions, key=lambda x: x[0])]
 
     for next_position in sorted_next_positions:
         next_order = board[current_position[0]][current_position[1]]['order'] + 1
         next_board = copy.deepcopy(board)
-        next_board[next_position[0]][next_position[1]]['is_pass'] = True
+        next_board[next_position[0]][next_position[1]]['is_passed'] = True
         next_board[next_position[0]][next_position[1]]['order'] = next_order
         ret = solve(next_board, next_position, initial_position)
         if ret is not None:
@@ -115,7 +88,7 @@ def print_board(board):
 def main():
     board = init_board()
     initial_position = get_initial_position()
-    board[initial_position[0]][initial_position[1]]['is_pass'] = True
+    board[initial_position[0]][initial_position[1]]['is_passed'] = True
     board[initial_position[0]][initial_position[1]]['order'] = 1
 
     print('----- initial board -----')
