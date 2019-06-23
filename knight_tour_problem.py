73ddaf08b6b0c6ed57baea0a5a1dc93df6367ee2
tona0516 <tonango.0516.gmail.com>
2019-06-23 15:17:36 +0900

    refactor: リファクタリング

--- knight_tour_problem.py
+++ knight_tour_problem.py
@@ -1,36 +1,62 @@
 import copy
 
-MASS_NUM = 8
+BOARD_LENGTH = 8
 
 
 def init_board():
-    mass = {
-        'is_passed': False,
-        'order': 0,
-    }
-    return [[copy.deepcopy(mass) for i in range(MASS_NUM)] for j in range(MASS_NUM)]
+    """ボードの初期化
+
+    Returns:
+        list -- マスの2次元配列
+    """
+    return [[0 for i in range(BOARD_LENGTH)] for j in range(BOARD_LENGTH)]
 
 
 def get_initial_position():
+    """初期座標を返却
+
+    Returns:
+        tuple -- 座標
+    """
     return (0, 0)
 
 
 def is_finished(board, current_posiotion, initial_position):
-    # 現在位置から初期位置に戻れること
-    next_positions = get_next_positions(board, current_posiotion)
+    """終了判定
+
+    Arguments:
+        board {list} -- ボード
+        current_posiotion {tuple} -- 現在座標
+        initial_position {tuple} -- 初期座標
+
+    Returns:
+        bool -- 解が見つかったらTrue
+    """
+    # 現在座標から初期座標に戻れること
+    next_positions = get_next_positions(board, current_posiotion, enable_passed_check=False)
     if not (initial_position in next_positions):
         return False
 
     # すべて通過していること
     for row in board:
-        for mass in row:
-            if mass['is_passed'] is False:
+        for square in row:
+            if square == 0:
                 return False
 
     return True
 
 
-def get_next_positions(board, current_position):
+def get_next_positions(board, current_position, enable_passed_check=True):
+    """現在座標から移動可能な座標を返却
+
+    Arguments:
+        board {list} -- ボード
+        current_position {tuple} -- 現在座標
+        enable_passed_check {bool} -- Trueなら通過した座標を除外
+    Returns:
+        list -- 座標のリスト
+    """
+    # 移動可能な座標を算出
     diffs = [
         (1, 2),
         (1, -2),
@@ -42,35 +68,59 @@ def get_next_positions(board, current_position):
         (-2, -1),
     ]
     tmp = [(current_position[0] + diff[0], current_position[1] + diff[1]) for diff in diffs]
-    next_positions = [np for np in tmp if 0 <= np[0] < MASS_NUM and 0 <= np[1] < MASS_NUM]
-    return next_positions
+    next_positions = [np for np in tmp if 0 <= np[0] < BOARD_LENGTH and 0 <= np[1] < BOARD_LENGTH]
+
+    if enable_passed_check is False:
+        return next_positions
+
+    # すでに通過したマスを除外
+    not_passed_next_positions = [np for np in next_positions if board[np[0]][np[1]] == 0]
+    return not_passed_next_positions
 
 
-def get_jumpable_positions(board, current_position):
-    next_positions = get_next_positions(board, current_position)
-    jumpable_positions = [np for np in next_positions if board[np[0]][np[1]]['is_passed'] is False]
-    return jumpable_positions
+def sort_by_Warnsdorfs_rule(board, next_positions):
+    """Warnsdorf's ruleに則ってソートする
+
+    Arguments:
+        board {list} -- ボード
+        next_positions {list} -- 次に移動する座標のリスト
+
+    Returns:
+        list -- ソートした次に移動する座標のリスト
+    """
+    tmp = [(len(get_next_positions(board, np)), np) for np in next_positions]
+    return [np[1] for np in sorted(tmp, key=lambda x: x[0])]
 
 
 def solve(board, current_position, initial_position):
+    """再帰的に探索
+
+    Arguments:
+        board {list} -- ボード
+        current_position {tuple} -- 現在座標
+        initial_position {tuple} -- 初期座標
+
+    Returns:
+        list -- ボード
+    """
+    # 解が見つかったら終了
     if is_finished(board, current_position, initial_position):
         return board
 
-    next_positions = get_jumpable_positions(board, current_position)
+    # 移動可能な座標を列挙
+    # なければ一手戻る
+    next_positions = get_next_positions(board, current_position)
     if not next_positions:
         return None
 
-    sorted_next_positions = []
-    for np in next_positions:
-        sorted_next_positions.append((len(get_jumpable_positions(board, np)), np))
+    # ソート
+    sorted_next_positions = sort_by_Warnsdorfs_rule(board, next_positions)
 
-    sorted_next_positions = [np[1] for np in sorted(sorted_next_positions, key=lambda x: x[0])]
-
+    # 移動して次手へ
     for next_position in sorted_next_positions:
-        next_order = board[current_position[0]][current_position[1]]['order'] + 1
+        next_order = board[current_position[0]][current_position[1]] + 1
         next_board = copy.deepcopy(board)
-        next_board[next_position[0]][next_position[1]]['is_passed'] = True
-        next_board[next_position[0]][next_position[1]]['order'] = next_order
+        next_board[next_position[0]][next_position[1]] = next_order
         ret = solve(next_board, next_position, initial_position)
         if ret is not None:
             return ret
@@ -79,17 +129,21 @@ def solve(board, current_position, initial_position):
 
 
 def print_board(board):
+    """ボードを標準出力
+
+    Arguments:
+        board {list} -- ボード
+    """
     for row in board:
-        for mass in row:
-            print("{0:2d}".format(mass['order']), end=' ')
+        for square in row:
+            print("{0:2d}".format(square), end=' ')
         print()
 
 
 def main():
     board = init_board()
     initial_position = get_initial_position()
-    board[initial_position[0]][initial_position[1]]['is_passed'] = True
-    board[initial_position[0]][initial_position[1]]['order'] = 1
+    board[initial_position[0]][initial_position[1]] = 1
 
     print('----- initial board -----')
     print_board(board)
